e0551288298f8f21e795cdfe2df5f041deac984c
Piyawat Malee <malee.piyawat@gmail.com>
2025-08-22 13:44:36 +0700

    refactor: replace axios calls with custom hook for fetching blog posts in HomePage and ViewPostPage

---
+++ client/src/hooks/useBlogPosts.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function useBlogPosts() {
+  const [posts, setPosts] = useState([]);
+  const [isError, setIsError] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+
+  const getPosts = async () => {
+    try {
+      setIsError(false);
+      setIsLoading(true);
+      const results = await axios("http://localhost:4000/posts");
+      setPosts(results.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setIsError(true);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  return {
+    posts,
+    isLoading,
+    isError
+  };
+}
+
+export default useBlogPosts;
